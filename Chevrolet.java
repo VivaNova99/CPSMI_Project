3f653ad08b92cb6be1d05ac421b9b4a631aef960
SobolevaElena <crazy_hedgehog13@mail.ru>
2015-11-04 20:21:22 +0300

    Soboleva Commit

---
+++ Chevrolet.java
@@ -0,0 +1,8 @@
+/**
+ * Created by sf2013 on 22.04.15.
+ */
+public class Chevrolet extends Jeep {
+    public void introdution(){
+        System.out.println("Это внедорожник марки Chevrolet");
+    }
+}
---
+++ Jeep.java
@@ -0,0 +1,10 @@
+/**
+ * Created by sf2013 on 22.04.15.
+ */
+public class Jeep extends PassengerCar {
+    @Override
+    public void introdution(){
+        System.out.println("Это внедорожник");
+    }
+}
+
