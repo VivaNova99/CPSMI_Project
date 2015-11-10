f848d2226b9fe552d1fa1adc4d4c8aa5a5f7e933
ElizavetaTomaeva <lizvladt@mail.ru>
2015-11-10 19:05:30 +0300

    Delete PassengerCar.java

--- PassengerCar.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by sf2013 on 22.04.15.
- */
-public class PassengerCar extends Car {
-    @Override
-    public void introdution(){
-        System.out.println("Это легковой автомобиль");
-    }
-}
