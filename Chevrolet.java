6e571dbba94668c800ee59e30f4ff3639b207cd7
ElizavetaTomaeva <lizvladt@mail.ru>
2015-11-10 19:00:16 +0300

    Delete Chevrolet.java

--- Chevrolet.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by sf2013 on 22.04.15.
- */
-public class Chevrolet extends Jeep {
-    public void introdution(){
-        System.out.println("Это внедорожник марки Chevrolet");
-    }
-}
